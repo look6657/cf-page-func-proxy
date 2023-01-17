723ebfae1ff4fbef68797f03b5dc0e1a837772a9
look6657 <92175650+look6657@users.noreply.github.com>
2023-01-18 01:50:56 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="example.com";
+        url.hostname="80-bf3f80d9a4734df6a39914d3e9a42397.patr.cloud";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
